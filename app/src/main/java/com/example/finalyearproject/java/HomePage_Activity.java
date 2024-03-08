12cee95ba1c6b1f17961a8f6670bb09b0ccff649
amar-rakhpasre <rakhpasreamar@gmai.com>
2024-03-08 20:38:19 +0530

    Describe your changes here

--- app/src/main/java/com/example/finalyearproject/java/HomePage_Activity.java
+++ app/src/main/java/com/example/finalyearproject/java/HomePage_Activity.java
@@ -1,6 +1,5 @@
 package com.example.finalyearproject.java;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import androidx.fragment.app.Fragment;
@@ -9,62 +8,46 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 import com.example.finalyearproject.R;
 import com.example.finalyearproject.databinding.ActivityHomePageBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomePage_Activity extends AppCompatActivity {
+public class HomePage_Activity extends AppCompatActivity  {
 
-    private BottomNavigationView bottomNavigationView;
-    private FrameLayout frameLayout;
-
+    ActivityHomePageBinding binding;
+    @SuppressLint("NonConstantResourceId")
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_home_page);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
 
-                bottomNavigationView = findViewById(R.id.bottomAppBar);
-                frameLayout = findViewById(R.id.frame_layout);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId(); // Get the ID once
 
-                bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                        int itemId = item.getItemId();
-
-                        if (itemId == R.id.navHome){
-                            loadFragment(new HomeFragment(), false);
-                        } else if (itemId == R.id.navBooking) {
-                            loadFragment(new MyBookingFragment(), false);
-                        } else if (itemId == R.id.navVoiceCommand) {
-                            loadFragment(new VoiceCommandFragment(), false);
-                        } else if (itemId == R.id.navInBox) {
-                            loadFragment(new InboxFragment(), false);
-                        } else if (itemId == R.id.navProfile) {
-                            loadFragment(new ProfileFragment(), false);
-                        }
-                        loadFragment(new HomeFragment(), true);
-
-                        return true;
-                    }
-                });
+            if (itemId == R.id.navHome) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.navBooking) {
+                replaceFragment(new MyBookingFragment());
+            } else if (itemId == R.id.navVoiceCommand) {
+                replaceFragment(new VoiceCommandFragment());
+            } else if (itemId == R.id.navInBox) {
+                replaceFragment(new InboxFragment());
+            } else if (itemId == R.id.navProfile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
 
     }
 
-    private void loadFragment(Fragment fragment, boolean isAppInitialized){
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        if (isAppInitialized){
-            fragmentTransaction.add(R.id.frame_layout, fragment);
-        } else if (isAppInitialized) {
-            fragmentTransaction.replace(R.id.frame_layout, fragment);
-        }
-
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
 }
